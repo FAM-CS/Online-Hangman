09ea0d7b574011e371e0eaa291787de3a5e971a9
Fatima Moussaoui <fatima.a.moussaoui@gmail.com>
2023-02-13 00:44:42 -0800

    Added anim func  and basic gameover/win

--- public/index.js
+++ public/index.js
@@ -12,10 +12,11 @@ var geuss_bank = document.querySelector(".guess-bank")
 const keyboard = document.querySelector(".keyboard")
 const keys = document.querySelectorAll(".keyboard button")
 
-// use .childNodes to access bits
-var word_container = document.querySelectorAll(".word-container .letter-guess")
-
+// WORD HANDLING
+var word_container = document.querySelectorAll(".word-container .letter-guess")     // use .childNodes to access bits
 var correct_word = "agile"
+var INCORRECT_TRIES = 0
+var REVEALED_LETTERS = 0
 
 
 keyboard.addEventListener("click", function(event) {
@@ -31,32 +32,70 @@ keyboard.addEventListener("click", function(event) {
 function handle_player_guess(guessed_letter) {
     // Check if letter is in correct word
     if (correct_word.includes(guessed_letter.toLowerCase())) {
-        guessed_letter = guessed_letter.toLowerCase()
-
-        word_container.forEach((curr_letter, index) => {
-            // console.log("LOOP: , i: ", curr_letter, index);
-            if (correct_word[index] == guessed_letter && curr_letter.textContent != guessed_letter) {
-                curr_letter.textContent = guessed_letter
-            }
-        })
+        correct_guess(guessed_letter.toLowerCase())
     }
     // Else, add to guess bank
     else {
-        var guesses = document.querySelector(".guess-bank")
-        var guesses_text = guesses.textContent
+        incorrect_guess(guessed_letter)
+    }
+}
 
-        // Only add letter if not already guessed
-        if (!guesses_text.includes(guessed_letter)) {
-            var unused_letter = document.createElement("div")
-            unused_letter.classList.add("unused-letter")
-            unused_letter.textContent = guessed_letter.toUpperCase()
 
-            geuss_bank.appendChild(unused_letter)
+function correct_guess(guessed_letter) {
+    word_container.forEach((curr_letter, index) => {
+        // console.log("LOOP: , i: ", curr_letter, index);
+        if (correct_word[index] == guessed_letter && curr_letter.textContent != guessed_letter) {
+            curr_letter.textContent = guessed_letter
+            REVEALED_LETTERS++
+
+            if (REVEALED_LETTERS === correct_word.length) {
+                alert("You won! Hit OK to play again")
+                reset_game()
+            }
+        }
+    })
+}
+
+
+function incorrect_guess(guessed_letter) {
+    // Only add letter if not already guessed
+    var guesses_text = document.querySelector(".guess-bank").textContent
+    //
+    if (!guesses_text.includes(guessed_letter)) {
+        var unused_letter = document.createElement("div")
+        unused_letter.classList.add("unused-letter")
+        unused_letter.textContent = guessed_letter.toUpperCase()
+
+        geuss_bank.appendChild(unused_letter)
+
+        // Update hangman image
+        INCORRECT_TRIES++
+        var hangman_img = document.getElementById("bob-id")
+        hangman_img.src = "/OUT/BobV" + INCORRECT_TRIES + ".png"
+        // console.log("IMG: ", hangman_img)
+
+        if (INCORRECT_TRIES === 6) {
+            alert("Gameover! Hit OK to play again")
+            reset_game()
         }
     }
 }
 
 
+function reset_game() {
+    INCORRECT_TRIES = 0
+    REVEALED_LETTERS = 0
+
+    var guesses = document.querySelector(".guess-bank")
+    guesses.textContent = ""
+
+    word_container.forEach(curr_letter => {
+            curr_letter.textContent = ""
+    })
+
+    var hangman_img = document.getElementById("bob-id")
+    hangman_img.src = "/OUT/BobV0.png"
+}
 
  /*********************************************************************
  ** Function:
